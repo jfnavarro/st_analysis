57da2bef55956638a5d928af61aca1b996c017d7
jfnavarro <jc.fernandez.navarro@gmail.com>
2016-03-11 14:48:08 +0100

    Adding first scripts

---
+++ convert_table_bc_to_coordinate.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+#@Author Jose Fernandez
+""" 
+Script that takes a matrix of counts
+where the columns are barcode Ids and convert
+the ids to XxY coordinates. For that it also needs
+a file with the barcode ids and the coordinates
+"""
+
+import argparse
+import sys
+import os
+import numpy as np
+import pandas as pd
+from stpipeline.common.utils import fileOk
+
+def main(counts_matrix, barcode_ids, outfile):
+
+    if not fileOk(counts_matrix) or len(barcode_ids) <= 0:
+        sys.stderr.write("Error, input file not present or invalid format\n")
+        sys.exit(-1)
+     
+    if not outfile:
+        outfile = "filtered_" + os.path.basename(counts_matrix)
+           
+    # loads all the barcodes
+    barcodes = dict()
+    with open(barcode_ids, "r") as filehandler:
+        for line in filehandler.readlines():
+            tokens = line.split()
+            barcodes[tokens[0]] = (tokens[1],tokens[2])
+    
+    counts_table = pd.read_table(counts_matrix, sep="\t", header=0)
+    transpose_counts_table = counts_table.transpose()
+    #transpose_counts_table = counts_table.transpose()
+    column_values = list(transpose_counts_table.columns.values)
+    new_column_values = list()
+    
+    for bc in column_values:
+        (x,y) = barcodes[bc]
+        new_column_values.append(x + "x" + y)
+    
+    transpose_counts_table.columns = new_column_values
+    transpose_counts_table.to_csv(outfile)
+               
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--counts-matrix",  
+                        help="Matrix with gene counts (genes as columns)")
+    parser.add_argument("--outfile", help="Name of the output file")
+    parser.add_argument("--barcodes-ids",
+                        help="File with the barcode ids and their coordinates")
+    args = parser.parse_args()
+    main(args.counts_matrix, args.barcodes_ids, args.outfile)
+
